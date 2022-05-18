f92ee3084d27f2dd011c4b6992d58933a783ca83
YurijKovbasyuk <kovbasyuk.yurij@gmail.com>
2022-05-18 14:59:02 +0300

    task 10 start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+console.log(searchBox);
+// https://restcountries.com/v3.1/name/{name}
